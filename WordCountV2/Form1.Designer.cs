14a1ce7a75a059160520d5bbcf1d4c1ededd608a
bdavi <b.davis_117@hotmail.ca>
2021-10-24 13:24:38 -0600

    Add project files.

---
+++ WordCountV2/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace WordCountV2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(21, 23);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(177, 13);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "Word Count: ";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox2.Cursor = System.Windows.Forms.Cursors.Default;
+            this.textBox2.Enabled = false;
+            this.textBox2.Location = new System.Drawing.Point(21, 42);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(177, 13);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.Text = "Char Count(no spaces): ";
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox3.Cursor = System.Windows.Forms.Cursors.Default;
+            this.textBox3.Enabled = false;
+            this.textBox3.Location = new System.Drawing.Point(21, 61);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(177, 13);
+            this.textBox3.TabIndex = 2;
+            this.textBox3.Text = "Char Count(inc. spaces): ";
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(83, 80);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Recount";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 5000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(240, 113);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Word Count";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ WordCountV2/Form1.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WordCountV2
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        //create string and fill it with whatever text is in the clipboard
+        public string wordsToCount;
+        public char firstLetter, lastLetter;
+        //create containers for number of words, letters, spaces, and letters+spaces
+        public int numberOfWords, numberOfLetters, numberOfSpaces, numberOfCharacters;
+
+        //this test sentence includes 11 words, 66 characters, and 10 spaces
+        //this test sentence includes 12 words, 70 characters, and 11 spaces now
+        //another test sentence with different letters
+        
+
+        
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        //function is called every time button1 is clicked
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            GetCount();
+
+            //set the text in each text box to correspond to its calculated value
+            this.textBox1.Text = "Word Count: " + numberOfWords;
+            this.textBox2.Text = "Char Count(no spaces): " + numberOfLetters;
+            this.textBox3.Text = "Char Count(inc. spaces): " + wordsToCount.Length;
+            
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        //function is called as soon as the application loads
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+            GetCount();
+
+            this.textBox1.Text = "Word Count: " + numberOfWords;
+            this.textBox2.Text = "Char Count(no spaces): " + numberOfLetters;
+            this.textBox3.Text = "Char Count(inc. spaces): " + wordsToCount.Length;
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+            TrackLetters();
+            wordsToCount = Clipboard.GetText();
+
+            if (wordsToCount[0] != firstLetter || wordsToCount[wordsToCount.Length - 1] != lastLetter)
+            {
+                GetCount();
+                this.textBox1.Text = "Word Count: " + numberOfWords;
+                this.textBox2.Text = "Char Count(no spaces): " + numberOfLetters;
+                this.textBox3.Text = "Char Count(inc. spaces): " + wordsToCount.Length;
+
+            }
+            
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        //function to calculate word and letter counts
+        private void GetCount()
+        {
+            //gather whatever text is on the clipboard
+            wordsToCount = Clipboard.GetText();
+            
+            //count all characters, including spaces in wordsToCount and returns the result as an int
+            numberOfCharacters = wordsToCount.Length;
+            //assume at least one word, since the first character won't be a space
+            numberOfWords = 1;
+            //start space count at zero
+            numberOfSpaces = 0;
+
+            
+
+            //run through each character in wordsToCount
+            for (int i = 1; i < wordsToCount.Length; i++)
+            {
+                if (char.IsWhiteSpace(wordsToCount[i]))//check if the character at position i is a space
+                {
+                    numberOfSpaces++;//increment space count by one
+
+                   
+                }
+
+                if (!char.IsWhiteSpace(wordsToCount[i]))//check if the character at position i is a letter or number
+                {
+                    if (char.IsWhiteSpace(wordsToCount[i - 1]))//checks if the previous character is a space
+                    {
+                        numberOfWords++;//increment word count by one
+                    }
+                }
+
+            }
+
+
+
+            //subtract spaces from all characters to get the number of letters
+            numberOfLetters = numberOfCharacters - numberOfSpaces;
+            TrackLetters();
+        }
+
+        private void TrackLetters()
+        {
+            firstLetter = wordsToCount[0];
+            lastLetter = wordsToCount[wordsToCount.Length - 1];
+
+            Console.WriteLine(firstLetter);
+            Console.WriteLine(lastLetter);
+        }
+    }
+}
